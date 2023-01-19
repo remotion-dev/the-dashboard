82b26acd1c96d4146d680b03ff59f94311e6fe19
Jonny Burger <jonathanburger11@gmail.com>
2023-01-19 10:37:45 +0100

    lunchmaster on thursday is different

--- components/LunchMandate.tsx
+++ components/LunchMandate.tsx
@@ -14,7 +14,11 @@ const container: React.CSSProperties = {
 
 export const LunchMandate = () => {
   const date = useDate("daily");
-  const theme = useTheme();
+  const dayOfWeek = new Date(date).getDay();
+
+  // Mit&Ohne must already be ordered by 10:30 AM,
+  // Therefore the lunchmaster reveal date is earlier on Thursday.
+  const reveal = dayOfWeek === 4 ? ([10, 0] as const) : ([10, 30] as const);
   const company = useCompany();
 
   const lunchMandate = lunchRoulette.sort((a, b) => {
@@ -33,7 +37,7 @@ export const LunchMandate = () => {
   const hour = new Date().getHours();
   const minute = new Date().getMinutes();
 
-  if (hour < 10 || (hour === 10 && minute <= 30)) {
+  if (hour < reveal[0] || (hour === reveal[0] && minute <= reveal[1])) {
     return (
       <div
         style={{
@@ -55,7 +59,7 @@ export const LunchMandate = () => {
           <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 464c-114.7 0-208-93.31-208-208S141.3 48 256 48s208 93.31 208 208S370.7 464 256 464zM256 336c-18 0-32 14-32 32s13.1 32 32 32c17.1 0 32-14 32-32S273.1 336 256 336zM289.1 128h-51.1C199 128 168 159 168 198c0 13 11 24 24 24s24-11 24-24C216 186 225.1 176 237.1 176h51.1C301.1 176 312 186 312 198c0 8-4 14.1-11 18.1L244 251C236 256 232 264 232 272V288c0 13 11 24 24 24S280 301 280 288V286l45.1-28c21-13 34-36 34-60C360 159 329 128 289.1 128z" />
         </svg>
         <br></br>
-        The lunch master will be revealed at 10:30 AM.
+        The lunch master will be revealed at {reveal[0]}:{reveal[1]} AM.
       </div>
     );
   }
