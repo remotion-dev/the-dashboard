a0c802ce4b69828dd9520d5755551170e593aafb
rbb.93 <roman.burkard@tvc.simspace.com>
2023-01-31 17:37:33 +0100

    increase size

--- components/WatchingYou.tsx
+++ components/WatchingYou.tsx
@@ -52,12 +52,12 @@ export const WatchingYou = () => {
         >
           {positive > 0.5 ? 'Congrats on your' : 'Watch your'} KPIs {person1[1].name.split(" ")[0]}, you are {performance}% {positive > 0.5 ? 'ahead of' : 'behind'}  your peers.
         </h1>
-        <h4 style = {{fontWeight: 'bold', marginBottom: 24}}>
+        <h2 style = {{fontWeight: 'bold', marginBottom: 24}}>
           Use the following key-takeaway from our last performance workshop
-        </h4>
-        <h3>
+        </h2>
+        <h2>
         {`👍 ${randomMotivation[performance % 11]}`}
-        </h3>
+        </h2>
     </div>
     </div>
 
