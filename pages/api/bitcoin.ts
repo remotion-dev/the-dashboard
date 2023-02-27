e7f15ace7af0c905fc06bdb7eefbf1212c114513
Jonny Burger <jonathanburger11@gmail.com>
2023-02-27 17:26:19 +0100

    ETH

--- pages/api/bitcoin.ts
+++ pages/api/bitcoin.ts
@@ -10,7 +10,7 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const data = await fetch(
-    "https://query2.finance.yahoo.com/v8/finance/chart/BTC-USD"
+    "https://query2.finance.yahoo.com/v8/finance/chart/ETH-USD"
   );
   const response = await data.json();
 
