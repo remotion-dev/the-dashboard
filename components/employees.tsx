384643bd29f551d56cf7e124f61cea72d32af5dc
Jonny Burger <jonathanburger11@gmail.com>
2023-03-22 11:38:26 +0100

    add more employees

--- components/employees.tsx
+++ components/employees.tsx
@@ -2,52 +2,86 @@ type Employee = {
   name: string;
   picture: string;
   employeeOfTheMonth: string;
+  birthday: [number, number] | null;
 };
 
 export const forOneRedEmployees: Employee[] = [
   {
     name: "Luke Zirngibl",
     picture: "luke.jpg",
+    birthday: [11, 5],
     employeeOfTheMonth:
       "Luke was named employee of the month for his exceptional leadership and contributions to the company. As the most experienced developer and chief executive, he has played a key role in driving the success of the development team and the company as a whole. His wealth of knowledge and expertise have been invaluable in guiding the team to new heights, and he is always willing to go above and beyond to ensure that the team is meeting its goals and objectives. In addition to his technical skills, Luke is a strong leader who is always willing to listen to and support his team members. His dedication and hard work make him a valuable asset to the company, and we are proud to recognize him as our employee of the month.",
   },
   {
     name: "Matteo Gamba",
     picture: "matteo.jpeg",
+    birthday: [21, 1],
     employeeOfTheMonth:
       "Matteo was named employee of the month for his outstanding contributions to the development team. He consistently delivers high-quality work on time, and is always willing to go above and beyond to meet project deadlines. In addition to his technical expertise, Matteo is a valuable team player who is always willing to lend a helping hand and share his knowledge with his colleagues. His innovative ideas and solutions have helped drive the team's success, and we are proud to recognize him as our employee of the month.",
   },
   {
     name: "Isabell Fink",
     picture: "isabell.jpeg",
+    birthday: [21, 8],
     employeeOfTheMonth:
       "Isabell was selected as employee of the month for her exceptional design skills and her ability to consistently deliver high-quality work. She has a strong eye for aesthetics and is able to effectively translate her clients' vision into visually appealing designs. In addition, Isabell is a team player and is always willing to collaborate with her colleagues to achieve the best possible results.",
   },
   {
     name: "Corey Bothwell",
     picture: "corey.jpeg",
+    birthday: [23, 7],
     employeeOfTheMonth:
       "Corey was selected as employee of the month for his exceptional technical skills and his ability to consistently deliver high-quality code. He has a strong understanding of programming languages and is able to quickly and effectively solve complex problems. In addition, Corey is a team player and is always willing to lend a helping hand to his colleagues.",
   },
   {
     name: "Chiara Turel",
     picture: "chiara.jpeg",
+    birthday: [6, 10],
     employeeOfTheMonth:
       "Chiara was selected as employee of the month for her exceptional creativity and attention to detail in her illustrations, as well as her ability to consistently meet tight deadlines and deliver high-quality work.",
   },
   {
     name: "Roman Burkard",
     picture: "roman.jpeg",
+    birthday: [3, 5],
     employeeOfTheMonth:
       "Roman was selected as employee of the month for his outstanding leadership and management skills, as well as his ability to effectively communicate with and support his team. He has consistently demonstrated a strong dedication to the growth and development of his team, and has played a key role in the success of the company.",
   },
   {
     name: "Luisa Stückelberger",
     picture: "luisa.jpg",
+    birthday: [15, 4],
     employeeOfTheMonth:
       "Luisa was named employee of the month for her exceptional work performance and ability to handle any situation with poise and grace. She consistently delivers high-quality work on time, and is always willing to go above and beyond to meet project deadlines. In addition to her technical expertise, Luisa is known for her ability to handle difficult situations with calmness and professionalism. She has a great attitude and is always willing to lend a helping hand to her colleagues. Her contributions and positive attitude have been invaluable to the development team, and we are proud to recognize her as our employee of the month.",
   },
   {
+    name: "Annika Berger",
+    picture: "annika.jpeg",
+    birthday: [23, 3],
+    employeeOfTheMonth:
+      "As an industrial designer at For One Red, Annika has consistently demonstrated exceptional creativity, ingenuity, and a deep commitment to her work. Her designs have consistently met or exceeded the expectations of clients, and her contributions to the company have been instrumental in driving its success. In addition to her professional skills, Annika is also known for her positive attitude and sense of humor. She has a natural ability to make her colleagues feel at ease, and her infectious chuckle can lighten the mood in even the most stressful situations. Her upbeat personality has made her a favorite among both clients and coworkers, and she is always willing to lend a helping hand or offer advice when needed.",
+  },
+  {
+    name: "Tomás Massano",
+    picture: "tomas.jpeg",
+    birthday: [4, 6],
+    employeeOfTheMonth:
+      "Maria Pavlou has been recognized as the Employee of the Month at For One Red due to her exceptional skills as a Greek illustrator and UI designer. She has consistently demonstrated a keen eye for detail and an ability to create stunning visuals that are both aesthetically pleasing and functional. Maria's work as a UI designer has been particularly noteworthy, as she has a talent for creating intuitive and user-friendly interfaces that make complex tasks seem simple. Her designs have received high praise from clients and users alike, and have contributed to the success of many projects at For One Red.",
+  },
+  {
+    name: "Maria Pavlou",
+    picture: "maria.jpeg",
+    birthday: null,
+    employeeOfTheMonth:
+      "As a photographer, Tomas has an eye for detail and composition that is truly remarkable. He is able to capture the essence of a product or brand in his photos, and his work has been used in numerous marketing campaigns to great effect. In addition to his photography skills, Tomas is also an expert videographer who is able to create compelling video content that engages viewers and tells a story. His videos have been used in various marketing campaigns and have received high praise from clients and colleagues.",
+  },
+  {
+    name: "Fernando Jusdado",
+    picture: "fernando.jpeg",
+    birthday: [30, 4],
+    employeeOfTheMonth:
+      "Fernando Jusdado has been recognized as the Employee of the Month due to his outstanding work and positive attitude at For One Red. Fernando's experience working at CERN, one of the world's most renowned research institutions, has given him a deep understanding of complex technical concepts, which he brings to bear in his work as a member of the For One Red team.",
   },
 ];
 
@@ -55,18 +89,21 @@ export const remotionEmployees: Employee[] = [
   {
     name: "Jonny Burger",
     picture: "jonny.png",
+    birthday: [9, 4],
     employeeOfTheMonth:
       "Jonny was named employee of the month for his exceptional leadership and contributions to the company as the tech lead and chief executive. He has played a key role in driving the success of the development team and the company as a whole, using his technical expertise and leadership skills to guide the team towards meeting its goals and objectives. In addition to his technical skills, Jonny is a strong and supportive leader who is always willing to listen to and support his team members. His dedication to the company and his team have been invaluable, and we are proud to recognize him as our employee of the month.",
   },
   {
     name: "Mehmet Ademi",
     picture: "mehmet.png",
+    birthday: [11, 7],
     employeeOfTheMonth:
       "Mehmet was named employee of the month for securing the @remotion username on Twitter and Instagram as well as his outstanding contributions to the company as a business developer. He has played a key role in driving the success of the business development team, using his excellent communication and negotiation skills to secure new partnerships and contracts for the company. ",
   },
   {
     name: "Patric Salvisberg",
     picture: "patric.png",
+    birthday: [24, 9],
     employeeOfTheMonth:
       "Patric was named employee of the month for his outstanding work performance and contributions as a junior developer. Despite being new to the team, he has quickly proven himself to be a valuable asset with his technical skills and ability to learn and adapt. Patric consistently delivers high-quality work on time, and is always willing to go above and beyond to meet project deadlines. In addition to his technical expertise, Patric is a team player who is always willing to lend a helping hand and share his knowledge with his colleagues. His contributions and positive attitude have been invaluable to the development team, and we are proud to recognize him as our employee of the month.",
   },
